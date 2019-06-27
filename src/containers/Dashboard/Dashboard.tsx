3f1f2b373cfddaa8afcc362d159a9d1cd6b97561
Tom Belton <tombelton@hotmail.co.uk>
2019-06-27 14:00:11 +0200

    feat: updated addSongToQueue actions

--- src/containers/Dashboard/Dashboard.tsx
+++ src/containers/Dashboard/Dashboard.tsx
@@ -5,25 +5,29 @@ import { getUserStats } from '../../redux/actions/getUserStats'
 import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import UserStats from '../../components/UserStats/UserStats'
+import songQueue from '../../components/SongQueue/SongQueue'
 
 const Dashboard: React.FC<LoginProps> = () => {
 
   const userStats = useSelector((state: any) => state.userStatsReducer.userStats)
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     dispatch(getUserStats())
   }, [])
-  
+
   console.log(userStats)
 
   return (
     <div className="Dashboard">
       <h1>Dashboard</h1>
-      <StyledUserStats userStats={userStats}/>
+      <StyledUserStats userStats={userStats} />
     </div>
   );
 }
+
+const StyledUserStats = styled(UserStats)`
+    width: 500px;`
 
 const StyledUserStats = styled(UserStats)`
     width: 500px;`
