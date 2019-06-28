a5ad752c9782311672a799b531c777798423ecb1
Tom Belton <tombelton@hotmail.co.uk>
2019-06-28 13:03:51 +0200

    feat: finished google authentication and local token storage

--- src/redux/middleware/socket.tsx
+++ src/redux/middleware/socket.tsx
@@ -5,26 +5,27 @@ import { SongQueue } from '../../types';
 
 export const socket: Middleware<any, any, any> = ({ dispatch, getState }: any) => {
   let socket: SocketIOClient.Socket;
-  
+
   return (next: any) => (action: any) => {
     if (!action.socket) return next(action);
 
     if (action.socket.command === 'connect') {
-      const url =`${WS_BASE_URL}/codeworks`;
+      const url = `${WS_BASE_URL}/codeworks`;
       console.log('CONNECTING TO SOCKET', url);
       socket = client.connect(url);
-
+      
       socket.on('connect', () => {
+        console.log('CONNECTED');
         socket.on('updatedPlaylist', (playlist: SongQueue) => {
           dispatch({
             type: `UPDATED_LIST`,
             playlist
           });
         });
-  
+
       });
-  
-      socket.on('error', (error:any) => {
+
+      socket.on('error', (error: any) => {
         dispatch({
           type: `SOCKET_ERROR`,
           error
